3de34a3807e3b4b6a4240cf8ae739207cfaa6016
Dima Budaragin <d.budaragin+dev@gmail.com>
2025-10-27 18:25:40 +0000

    refactor FormEmail to only use props, not context for SSR rendering

--- src/components/form-email/FormEmail.tsx
+++ src/components/form-email/FormEmail.tsx
@@ -1,41 +1,39 @@
 import { CornerDownRight } from 'lucide-react'
 
-import useFormState, { type State, StateContext } from '@/components/form/useFormState'
+import { type State } from '@/components/form/useFormState'
 import { deriveFollowupId } from '@/lib/derive-ids'
 import { type Field, type Form, type Page, type SelectableField } from '@/types/form'
 
 export default function FormEmail({ form, data }: { form: Form, data: State }) {
   const pages = form.pages.flat()
   return (
-    <StateContext.Provider value={{ state: data, setState: () => { }, validate: () => true, currentPage: 0, pages: 0 }}>
-      <div className="flex flex-col gap-4 max-w-4xl bg-white p-8" style={{ fontFamily: 'sans-serif' }}>
-        {pages.map((page, i) => <RenderPage page={page} key={i} />)}
-      </div>
-    </StateContext.Provider>
-  )
-}
-
-function RenderPage({ page }: { page: Page }) {
-  const fieldsArr = page.sections.filter(section => section.type === 'fields').map(({ fields }) => fields)
-  return (
-    <div>
-      <h1 className="text-2xl bg-black text-white px-2 py-1 inline-block mb-4">{page.page}</h1>
-      {fieldsArr.map((fields, i) => <RenderFields fields={fields} key={i} />)}
+    <div className="flex flex-col gap-4 max-w-4xl bg-white p-8" style={{ fontFamily: 'sans-serif' }}>
+      {pages.map((page, i) => <RenderPage page={page} key={i} data={data} />)}
     </div>
   )
 }
 
-function RenderFields({ fields }: { fields: Field[] }) {
-  return <>{fields.map((field, i) => <RenderField field={field} key={i} />)}</>
+function RenderPage({ page, data }: { page: Page, data: State }) {
+  const fieldsArr = page.sections.filter(section => section.type === 'fields').map(({ fields }) => fields)
+  return (
+    <div>
+      <h1 className="text-2xl bg-black text-white px-2 py-1 inline-block mb-4">{page.page}</h1>
+      {fieldsArr.map((fields, i) => <RenderFields fields={fields} key={i} data={data} />)}
+    </div>
+  )
 }
 
-function RenderField({ field }: { field: Field }) {
+function RenderFields({ fields, data }: { fields: Field[], data: State }) {
+  return <div>{fields.map((field, i) => <RenderField field={field} key={i} data={data} />)}</div>
+}
+
+function RenderField({ field, data }: { field: Field, data: State }) {
   return (
-    <div className="flex border-b border-black mb-4 items-center">
-      <div className="w-3xs px-2 shrink-0">
+    <div className="flex mb-4 items-center" style={{ borderBottom: '1px solid #000' }}>
+      <div className="w-64 px-2 shrink-0">
         <RenderLabel field={field} />
       </div>
-      <div className="ml-8"><RenderValue field={field} /></div>
+      <div className="ml-8"><RenderValue field={field} data={data} /></div>
     </div>
   )
 }
@@ -45,52 +43,51 @@ function RenderLabel({ field }: { field: Field }) {
   const hasPlaceholder = (type === 'text' || type === 'select' || type === 'multi-select') && field.placeholder
   return (
     <div>
-      {label && !hasPlaceholder && <p className="leading-tight">{label}</p>}
-      {hasPlaceholder && <p className="leading-tight">{field.placeholder}</p>}
+      {label && !hasPlaceholder && <div className="leading-tight">{label}</div>}
+      {hasPlaceholder && <div className="leading-tight">{field.placeholder}</div>}
     </div>
   )
 }
 
-function RenderValue({ field }: { field: Field }) {
+function RenderValue({ field, data }: { field: Field, data: State }) {
   const { type, id } = field
-  const { state } = useFormState()
-  const value = state[id]
+  const value = data[id]
   const className = 'bg-amber-200 px-2 pt-1'
   switch (type) {
-    case 'text': return <p className={className}>{value as string}</p>
+    case 'text': return <div className={className}>{value as string}</div>
     case 'date-picker': {
       const date = new Date(value as string)
-      return <p className={className}>{date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
+      return <div className={className}>{date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</div>
     }
     case 'weight': return (
-      <p className={className}>
+      <div className={className}>
         {value as string}
         &nbsp;kg
-      </p>
+      </div>
     )
     case 'height': return (
-      <p className={className}>
+      <div className={className}>
         {value as string}
         &nbsp;cm
-      </p>
+      </div>
     )
     case 'select': return (
-      <>
+      <div>
         <span className={className}><RenderSelectedOption field={field} option={value as string} /></span>
-        <RenderFollowup field={field} option={value as string} />
-      </>
+        <RenderFollowup field={field} option={value as string} data={data} />
+      </div>
     )
     case 'multi-select': return (
-      <>
+      <div>
         <ul className="list-disc">
           {(value as string[]).map((option, i) => (
             <li key={i}>
               <span className={className}><RenderSelectedOption field={field} option={option} /></span>
-              <RenderFollowup field={field} option={option} />
+              <RenderFollowup field={field} option={option} data={data} />
             </li>
           ))}
         </ul>
-      </>
+      </div>
     )
   }
 }
@@ -103,7 +100,7 @@ function RenderSelectedOption({ field, option }: { field: SelectableField, optio
     return options[option]
 }
 
-function RenderFollowup({ field, option }: { field: SelectableField, option: string }) {
+function RenderFollowup({ field, option, data }: { field: SelectableField, option: string, data: State }) {
   const { followup } = field
   if (!followup) return null
   if (!followup[option]) return null
@@ -113,7 +110,7 @@ function RenderFollowup({ field, option }: { field: SelectableField, option: str
       <CornerDownRight className="text-slate-500 shrink-0" />
       <div className="mt-2">
         <RenderLabel field={next} />
-        <RenderValue field={next} />
+        <RenderValue field={next} data={data} />
       </div>
     </div>
   )
